0fcb4dfbf0f0f852fc65a3526a10056d47c1f54c
John Quinn <johnedquinn@gmail.com>
2020-05-28 17:16:38 -0400

    Completed Conversation component

--- src/components/Conversation.vue
+++ src/components/Conversation.vue
@@ -1,11 +1,34 @@
 <template>
     <md-app>
+      
       <md-app-toolbar class="md-primary">
+        <md-button class="md-icon-button" to="/conversations">
+            <md-icon>arrow_back</md-icon>
+        </md-button>
+        <md-avatar v-if="conversation.img && conversation.img != ''" class="md-avatar-icon">
+          <img :src="conversation.img" />
+        </md-avatar>
+        <md-avatar v-else class="md-large">
+          <md-icon>perm_identity</md-icon>
+        </md-avatar>
         <span class="md-title">Conversation</span>
       </md-app-toolbar>
+
       <md-app-content>
         <h1>Conversation #{{ id }}</h1>
-        <md-button class="md-raised" @click="login">Login</md-button>
+        <div class="form" style="padding: 2em;">
+          <md-field>
+              <label>Name</label>
+              <md-input v-model="conversation.name"></md-input>
+          </md-field>
+          <md-avatar v-if="conversation.img && conversation.img != ''" class="md-avatar-icon">
+            <img :src="conversation.img" />
+          </md-avatar>
+          <input id="file" @change="saveImage" type="file" accept="image/*" style="display:none;" />
+          <md-button class="md-primary" @click="openImage">Set Image</md-button>
+          <md-button class="md-primary" @click="accept">Accept</md-button>
+          <md-button class="md-primary" to="/conversations">Cancel</md-button>
+        </div>
       </md-app-content>
     </md-app>
 </template>
@@ -21,28 +44,51 @@ export default {
     }
   },
   mounted() {
- if (this.id) {
- this.$db.listConversations(
- this.$user.token,
- { id: this.id },
- (err, convs) => {
- if (err) alert(err.message);
- else {
- for (var id in convs[0]) {
- Vue.set(this.conversation, id, convs[0][id]);
- }
- }
- }
- );
- }
+    if (this.id) {
+      this.getConvInfo();
+    }
  },
   methods: {
-    login() {
-      this.$db.login('john', 'john', (err, token, user) => {
-        if (err) this.msg = err.message;
-        else this.msg = JSON.stringify(user);
-      });
-    }
+    getConvInfo () {
+      this.$db.listConversations(
+        this.$user.token,
+        { id: this.id },
+        (err, convs) => {
+          if (err) alert(err.message);
+          else {
+            for (var id in convs[0]) {
+              Vue.set(this.conversation, id, convs[0][id]);
+            }
+          }
+        }
+      );
+    },
+    accept() {
+            console.log("Conversation.accept()");
+            this.$db.updateConversation(this.$user.token, this.conversation, (err, conv) => {
+                if (err) alert(err.message);
+                else {
+                    //this.$conversation.name = conv.name;
+                    //this.$conversation.img = conv.img;
+                    this.$router.back();
+                }
+            });
+        },
+        openImage(event) {
+            console.log("Conversation.openImage()");
+            document.getElementById("file").click();
+        },
+        saveImage() {
+            console.log("Conversation.saveImage()");
+            var self = this;
+            var element = document.getElementById("file");
+            var file = element.files[0];
+            var reader = new FileReader();
+            reader.onloadend = function() {
+                Vue.set(self.conversation, "img", reader.result);
+            };
+            reader.readAsDataURL(file);
+        }
   }
 }
 </script>
